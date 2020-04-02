0fb6457c65ec6126a29625f85e90948789528bbf
muezwink <muezwink@gmail.com>
2020-04-03 07:27:57 +0900

    Hello Node

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
-const fs = require('fs');
+const http = require('http');
 
-const data = fs.readFile('./data.txt', 'utf8', function(err, data) {
-    console.log(data);
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World\n');
 });
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
+
+// curl -X GET 'localhost:3000'
