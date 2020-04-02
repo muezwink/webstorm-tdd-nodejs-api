fb18f30e9f3136fd852c5a55d2718850518bbdb3
muezwink <muezwink@gmail.com>
2020-04-03 07:44:12 +0900

    미들웨어 실행 순서

--- index.js
+++ index.js
@@ -3,9 +3,16 @@ const app = express();
 
 function logger(req, res, next) {
     console.log('i am logger');
+    next();
+}
+
+function logger2(req, res, next) {
+    console.log('i am logger2');
+    next();
 }
 
 app.use(logger);
+app.use(logger2);
 
 app.listen(3000, function() {
    console.log('Server is running');
