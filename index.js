f46bd541d27ecc0f8ce7d36f4652ee2522ff0475
muezwink <muezwink@gmail.com>
2020-04-03 01:08:27 +0900

    readFileSync

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
-const math =  require('./math.js');
-const result = math.sum(1, 2);
-console.log(result);
+const fs = require('fs');
+
+// const data = fs.readFileSync('./data.txt');
+const data = fs.readFileSync('./data.txt', 'utf8');
+
+console.log(data);
+// jsonmraz:webstorm-tdd-nodejs-api mraz$ node index.js
+// <Buffer 54 68 69 73 20 69 73 20 64 61 74 61 20 66 69 6c 65>
 
 // jsonmraz:webstorm-tdd-nodejs-api mraz$ node index.js
-// 3
+// This is data file
